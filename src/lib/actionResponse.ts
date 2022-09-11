772b5f6ef852d3de2a4b5af0b1ede38cdbc9fa78
Laxsjo <rasmus.soderhielm@gmail.com>
2022-09-11 12:11:30 +0200

    add actionResponse library of sorts

---
+++ src/lib/actionResponse.ts
@@ -0,0 +1,167 @@
+export type InvResponseType = 'missing' | 'type' | 'value' | 'format' | 'id';
+
+type MaybeArray<T, Condition extends boolean> = Condition extends true ? T[] : T;
+
+interface InvResponse<T extends InvResponseType, Multiple extends boolean> {
+	type: T;
+	params: MaybeArray<string, Multiple>;
+	values?: MaybeArray<string, Multiple>;
+	messages?: MaybeArray<string, Multiple>;
+}
+
+interface InvMissingResponse<Multiple extends boolean> extends InvResponse<'missing', Multiple> {
+	values: undefined;
+	messages: undefined;
+}
+
+interface InvTypeResponse<Multiple extends boolean> extends InvResponse<'type', Multiple> {
+	values: MaybeArray<string, Multiple>;
+	messages: undefined;
+	expectedTypes: MaybeArray<string, Multiple>;
+}
+
+interface InvValueResponse<Multiple extends boolean> extends InvResponse<'value', Multiple> {
+	values: MaybeArray<string, Multiple>;
+	messages: MaybeArray<string, Multiple>;
+}
+
+interface InvFormatResponse<Multiple extends boolean> extends InvResponse<'format', Multiple> {
+	values: MaybeArray<string, Multiple>;
+	messages: MaybeArray<string, Multiple>;
+}
+
+interface InvIdResponse<Multiple extends boolean> extends InvResponse<'id', Multiple> {
+	values: MaybeArray<string, Multiple>;
+	messages: undefined;
+}
+
+/**
+ * Generate invalid _missing_ parameter response object
+ *
+ * This should be returned when **params** weren't submitted with the request.
+ *
+ * When given multiple parameters, the response object only states that _any_
+ * of the given parameters were invalid.
+ *
+ * @param params
+ * @returns
+ */
+export function invalidMissing<Multiple extends boolean>(
+	params: MaybeArray<string, Multiple>
+): InvMissingResponse<Multiple> {
+	return {
+		type: 'missing',
+		params,
+		values: undefined,
+		messages: undefined
+	};
+}
+
+/**
+ * Generate invalid parameter _type_ response object
+ *
+ * This should be returned when **params** couldn't be converted to excpected **types**.
+ *
+ * When given multiple parameters, the response object only states that _any_
+ * of the given parameters were invalid.
+ *
+ * @param params
+ * @param values
+ * @param types
+ * @returns
+ */
+export function invalidType<Multiple extends boolean>(
+	params: MaybeArray<string, Multiple>,
+	values: MaybeArray<string, Multiple>,
+	types: MaybeArray<string, Multiple>
+): InvTypeResponse<Multiple> {
+	return {
+		type: 'type',
+		params,
+		values,
+		messages: undefined,
+		expectedTypes: types
+	};
+}
+
+/**
+ * Generate invalid parameter _value_ response object
+ *
+ * This should be returned when the **values** of **params** aren't valid,
+ * e.g. when a number is to large.
+ * The constraints of the value should be specified in **messages**.
+ *
+ * When given multiple parameters, the response object only states that _any_
+ * of the given parameters were invalid.
+ *
+ * @param params
+ * @param values
+ * @param messages
+ * @returns
+ */
+export function invalidValue<Multiple extends boolean>(
+	params: MaybeArray<string, Multiple>,
+	values: MaybeArray<string, Multiple>,
+	messages: MaybeArray<string, Multiple>
+): InvValueResponse<Multiple> {
+	return {
+		type: 'value',
+		params,
+		values,
+		messages
+	};
+}
+
+/**
+ * Generate invalid parameter _format_ response object
+ *
+ * This should be returned when the **values** of **params** doesn't follow any
+ * valid format, such as an incorrect email address.
+ * The format of the value should be specified in **messages**.
+ *
+ * When given multiple parameters, the response object only states that _any_
+ * of the given parameters were invalid.
+ *
+ * @param params
+ * @param values
+ * @param messages
+ * @returns
+ */
+export function invalidFormat<Multiple extends boolean>(
+	params: MaybeArray<string, Multiple>,
+	values: MaybeArray<string, Multiple>,
+	messages: MaybeArray<string, Multiple>
+): InvFormatResponse<Multiple> {
+	return {
+		type: 'format',
+		params,
+		values,
+		messages
+	};
+}
+
+/**
+ * Generate invalid parameter with _non-existent id_ response object
+ *
+ * This should be returned when the value of the given parameter points to a
+ * non-existant reference, such as an when given a username that doesn't
+ * exist.
+ *
+ * When given multiple parameters, the response object only states that _any_
+ * of the given parameters were invalid.
+ *
+ * @param params
+ * @param values
+ * @returns
+ */
+export function invalidId<Multiple extends boolean>(
+	params: MaybeArray<string, Multiple>,
+	values: MaybeArray<string, Multiple>
+): InvIdResponse<Multiple> {
+	return {
+		type: 'id',
+		params,
+		values,
+		messages: undefined
+	};
+}
